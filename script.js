d814f250ae37df64ad25bb63794bc3f02ce8a45c
Vijay Chandravanshi <168979749+Vijay-Chandravanshi@users.noreply.github.com>
2025-09-26 08:17:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //check email caterina 
 
-let email = document.getElementById('email');
-let password = document.getElementById('password');
+let loginEmail = document.getElementById('loginEmail');
+let loginPassword = document.getElementById('loginPassword');
+//login inputs
+
+let signUpName = document.getElementById('signUpName');
+let signUpDOB = document.getElementById('signUpDOB')
+let signUpEmail = document.getElementById('signUpEmail');
+let signUpPassword = document.getElementById('signUpPassword');
+//signUp input 
 let status = document.getElementById('status');
 function login() {
-    let passwordValue = password.value;
+    let passwordValue = loginPassword.value;
     /*
     console.log(emailPattern.test(email.value));
     console.log(password.value);
     console.log(password.value.length);*/
-    if (email.value == ""){
+    if (loginEmail.value == ""){
         status.innerHTML="Please, enter email";
     status.style.color = 'red';
     }else if(passwordValue == "" ){
     status.innerHTML="Please, enter password";
     status.style.color = 'red';
-    }else if (!emailPattern.test(email.value)) {
+    }else if (!emailPattern.test(loginEmail.value)) {
     status.innerHTML="Invalid email";
     status.style.color = 'red';
 }else if(passwordValue.length < 8 || passwordValue.length > 20){
@@ -32,3 +39,13 @@ function signUp() {
     status.innerHTML = "Sign up....";
     status.style.color = "green";
 };
+// Toggle forms
+document.getElementById("showSignUp").addEventListener("click", () => {
+  loginForm.classList.add("hidden");
+  signUpForm.classList.remove("hidden");
+});
+
+document.getElementById("showLogin").addEventListener("click", () => {
+  signUpForm.classList.add("hidden");
+  loginForm.classList.remove("hidden");
+});
